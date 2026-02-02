5d74c853772a666c4e937a31133d68c61731f50b
akifcankara <akifcan@techtile.com.tr>
2026-02-02 11:49:34 +0300

    revert: company sidemenu item

--- components/common/side-menu/clients-section.tsx
+++ components/common/side-menu/clients-section.tsx
@@ -12,7 +12,7 @@ export default function ClientsSection() {
         Clients
       </div>
       <AllClientsMenuItem />
-      <CompaniesMenuItem />
+      {/* <CompaniesMenuItem /> */}
     </div>
   );
 }
